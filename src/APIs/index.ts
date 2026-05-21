97b06254c6b78d4e762768faa52abd647058c832
NiyakoA <116742750+NiyakoA@users.noreply.github.com>
2026-05-21 15:44:16 -0400

    feat: ✨ register exam route in API router

--- src/APIs/index.ts
+++ src/APIs/index.ts
@@ -5,12 +5,14 @@ import General from './router'
 import authRoutes from './user/authentication'
 import userManagementRoutes from './user/management'
 import ocrRoutes from './ocr'
+import examRoutes from './exam'
 
 const App = (app: Application) => {
     app.use(`${API_ROOT}`, General)
     app.use(`${API_ROOT}`, authRoutes)
     app.use(`${API_ROOT}/user`, userManagementRoutes)
     app.use(`${API_ROOT}`, ocrRoutes)
+    app.use(`${API_ROOT}`, examRoutes)
 }
 
 export default App
