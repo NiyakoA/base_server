2b212036f2ba1e731996132d6aaed4b1e57895ea
NiyakoA <116742750+NiyakoA@users.noreply.github.com>
2026-05-25 13:01:06 -0400

    fix: 🐛 add index on userId in Test model for query performance

--- src/APIs/exam/test.model.ts
+++ src/APIs/exam/test.model.ts
@@ -3,7 +3,7 @@ import { ITest } from './types/exam.interface'
 
 const testSchema = new mongoose.Schema<ITest>(
     {
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
         name: { type: String, required: true, trim: true }
     },
     { timestamps: true }
